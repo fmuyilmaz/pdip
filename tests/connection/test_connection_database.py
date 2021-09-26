526abee6ef70c3796c1806f8c7c2b54699f2b7d5
Ahmet Çağrı AKCA <ahmetcagriakca@gmail.com>
2021-09-26 04:32:18 +0300

    remove connection tests

--- tests/connection/test_connection_database.py
+++ tests/connection/test_connection_database.py
@@ -11,9 +11,9 @@ class TestConnectionDatabase(TestCase):
     def tearDown(self):
         pass
 
-    def test_database_context_postgres_connect(self):
-        self.database_context = DatabaseProvider().get_context(connection_type=ConnectionTypes(1),
-                                                               connector_type=ConnectorTypes(3),
-                                                               host='localhost', port='5432', user='postgres',
-                                                               password='123456', database='test_pdi')
-        self.database_context.connector.connect()
+    # def test_database_context_postgres_connect(self):
+    #     self.database_context = DatabaseProvider().get_context(connection_type=ConnectionTypes(1),
+    #                                                            connector_type=ConnectorTypes(3),
+    #                                                            host='localhost', port='5432', user='postgres',
+    #                                                            password='123456', database='test_pdi')
+    #     self.database_context.connector.connect()
