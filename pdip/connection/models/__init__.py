d6d020dc4016ef96603600d4fe6e92aa01b2c313
Ahmet Çağrı AKCA <ahmetcagriakca@gmail.com>
2021-10-27 19:11:09 +0300

    add base converter

---
+++ pdip/connection/models/__init__.py
@@ -0,0 +1,1 @@
+from .data_queue_task import DataQueueTask
--- pdip/json/__init__.py
+++ pdip/json/__init__.py
@@ -1,2 +1,3 @@
 from .date_time_encoder import DateTimeEncoder
 from .json_convert import JsonConvert
+from .base_converter import BaseConverter
